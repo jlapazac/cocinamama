62d116c4689ae494364a5b7daaac35ff6d7f2355
Jose Luis Apaza Cueva <jlapazac@grupokonecta.com>
2022-03-30 14:35:25 -0500

    Revision de codigo para Merge

--- app/src/main/java/com/example/cocinamama/LoginActivity.java
+++ app/src/main/java/com/example/cocinamama/LoginActivity.java
@@ -6,19 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.example.cocinamama.ui.home.HomeFragment;
-
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
-
-    public void Login(View view) {
-
-        startActivity(new Intent(this, MenuActivity.class));
     }
 
     public void goRegister(View view) {
